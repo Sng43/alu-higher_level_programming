f58f05872be4b3af0822e979897f9aa28d671311
Sng43 <https://github.com/Sng43/MySuperRepo.git>
2024-02-22 17:44:26 -0800

    completed

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n:
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
+from sys import argv
 
 if __name__ == "__main__":
-    import sys
-    add_arg(sys.argv)
+    i = 1
+    result = 0
+    arguments = len(argv) - 1
+
+    if (arguments != 0):
+        while (i <= arguments):
+            result += int(argv[i])
+            i += 1
+    print(result)
