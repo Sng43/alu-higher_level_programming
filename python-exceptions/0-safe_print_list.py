f593a68684445367114e3325ab5cbe699b806b65
Sng43 <https://github.com/Sng43/MySuperRepo.git>
2024-02-22 20:39:58 -0800

    complete

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    idx = 0
-
+    num_of_elem = 0
+    len_list = 0
+    counter = 0
+    for item in my_list:
+        len_list += 1
     try:
-        for i in my_list:
-            if idx < x:
-                print('{}'.format(my_list[idx]), end='')
-                idx += 1
-
-        print()
-    except TypeError:
+        if (x == 0):
+            return num_of_elem
+        if (x >= len_list):
+            for elem in my_list:
+                print("{}".format(elem), end="")
+            print()
+            return len_list
+        else:
+            for elem in my_list:
+                num_of_elem += 1
+                print("{}".format(elem), end="")
+                if (num_of_elem == x):
+                    break
+            print()
+            return num_of_elem
+    except BaseException:
         pass
-    finally:
-        return idx
