6ea7bb6ba6739b7a81b0e2bb3959e15b00d97c14
Sng43 <https://github.com/Sng43/MySuperRepo.git>
2024-02-22 20:11:21 -0800

    complete

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,15 +1,6 @@
-
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    """muliply the value in a dictionary
-
-    Args:
-        a_dictionary: a dictionary
-
-    Returns:
-       the new dictionary with modified value
-    """
-    new_dict = {}
-    for i in a_dictionary:
-        new_dict[i] = a_dictionary[i] * 2
-    return (new_dict)
+    newC = a_dictionary.copy()
+    for i, j in newC.items():
+        newC[i] = j * 2
+    return (newC)
