ef750a4808be8f8ab96663dec749706b9680e4ca
SENGA KABARE <129498637+Sng43@users.noreply.github.com>
2024-03-24 22:29:57 +0200

    Update 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# display the body of a url call 
-curl -sG "$1" -H "X-School-User-Id: 98"
+# a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.
+curl -s "$1" -X GET -H "X-School-User-Id: 98"
